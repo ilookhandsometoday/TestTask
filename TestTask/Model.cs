f3d6d7241dae7ebc8d5f9e7e79022846d3efbaef
Никита Денисов <nikitadenisovvalerevich@gmail.com>
2020-03-08 22:09:48 +0500

    Implement(partially) DownloadImage

--- TestTask/Model.cs
+++ TestTask/Model.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System.Drawing;
+using System.IO;
 
 namespace TestTask
 {
     class Model
     {
-        public static void DownloadImage(string url) 
+        private HttpClient httpClient;
+        public async void DownloadImage(string url, string fileName) 
         {
-            
+            HttpRequestMessage get = new HttpRequestMessage(HttpMethod.Get, url);
+            HttpResponseMessage response = await this.httpClient.SendAsync(get, HttpCompletionOption.ResponseHeadersRead);
+            using (Stream stream = await response.Content.ReadAsStreamAsync()) 
+            {
+                stream.Length;
+            };
         }
     }
 }
