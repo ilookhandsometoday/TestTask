4d2e916148ef8e50d0633fb3c41424520fb6a82b
Никита Денисов <nikitadenisovvalerevich@gmail.com>
2020-03-08 13:34:47 +0500

    Create Model.cs

---
+++ TestTask/Model.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask
+{
+    class Model
+    {
+        public static void DownloadImage(string url) 
+        {
+            
+        }
+    }
+}
