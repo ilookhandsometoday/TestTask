b9e5e91831a6c831bd7cb5b2793afaa3be9ec3a7
Никита Денисов <nikitadenisovvalerevich@gmail.com>
2020-03-08 23:47:58 +0500

    Change the model

--- TestTask/Model.cs
+++ TestTask/Model.cs
@@ -12,14 +12,9 @@ namespace TestTask
     class Model
     {
         private HttpClient httpClient;
-        public async void DownloadImage(string url, string fileName) 
+        public async void GetContentLength(string url)
         {
-            HttpRequestMessage get = new HttpRequestMessage(HttpMethod.Get, url);
-            HttpResponseMessage response = await this.httpClient.SendAsync(get, HttpCompletionOption.ResponseHeadersRead);
-            using (Stream stream = await response.Content.ReadAsStreamAsync()) 
-            {
-                stream.Length;
-            };
+            
         }
     }
 }
