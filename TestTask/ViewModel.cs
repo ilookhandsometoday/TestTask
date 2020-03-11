b8ffb7b51fc0767d8e2a0b470f1cac80f68cbbd8
Никита Денисов <nikitadenisovvalerevich@gmail.com>
2020-03-11 21:35:11 +0500

    Implement ViewModel

--- TestTask/ViewModel.cs
+++ TestTask/ViewModel.cs
@@ -4,11 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel;
-using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
 
 namespace TestTask
 {
-    public class ViewModel/*: INotifyPropertyChanged*/
+    public class ViewModel: INotifyPropertyChanged
     {
+        private Downloader left, center, right;
+        public Downloader Left 
+        {
+            get { return this.left; }
+        }
+
+        public Downloader Center 
+        {
+            get { return this.center; }
+        }
+        public Downloader Right
+        {
+            get { return this.right; }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string property = "")
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
     }
 }
