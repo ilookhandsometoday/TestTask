5a0c070bf4ecdf6d050503d57b1e572fde6c5983
Никита Денисов <nikitadenisovvalerevich@gmail.com>
2020-03-13 20:11:13 +0500

    Remove the Position crutch

--- TestTask/MainWindow.xaml.cs
+++ TestTask/MainWindow.xaml.cs
@@ -23,6 +23,7 @@ namespace TestTask
         private Downloader LeftDownloader;
         private Downloader CenterDownloader;
         private Downloader RightDownloader;
+        private Progress<double> Progress;
 
         public MainWindow()
         {
@@ -30,38 +31,40 @@ namespace TestTask
             this.LeftDownloader = new Downloader();
             this.CenterDownloader = new Downloader();
             this.RightDownloader = new Downloader();
+            this.Progress = new Progress<double>(value => progressBar.Value = value);
             MessageBox.Show("Внимание! После нажатия кнопки \"Старт\"  или \"Загрузить всё\" проходит несколько секунд " +
                 "прежде чем загрузка действительно начнется. В это время кнопки \"Стоп\" будут неотзывчивыми");
         }
 
-        private enum Position
+        private async Task UpdateProgressBar()
         {
-            Left,
-            Center,
-            Right
+            do
+            {
+                await Task.Run(() =>
+                {
+
+                    if (LeftDownloader.State == Downloader.States.Downloading
+                    || CenterDownloader.State == Downloader.States.Downloading
+                    || RightDownloader.State == Downloader.States.Downloading)
+                    {
+                        double percentageValue = Convert.ToDouble
+                        (LeftDownloader.CurrentSize
+                        + CenterDownloader.CurrentSize
+                        + RightDownloader.CurrentSize)
+                        / Convert.ToDouble
+                        (LeftDownloader.ExpectedSize
+                        + CenterDownloader.ExpectedSize
+                        + RightDownloader.ExpectedSize) * 100L;
+                        ((IProgress<double>)Progress).Report(percentageValue);
+                    }
+                }
+                );
+            } while (true);
         }
 
-        private void PreventNoURLStart(Position position)
+        private void PreventNoURLStart(TextBox textBox, Button startButton)
         {
-            Button buttonWildCard = new Button();
-            TextBox textBoxWildCard = new TextBox();
-            switch (position)
-            {
-                case Position.Left:
-                    textBoxWildCard = textBoxURLLeft;
-                    buttonWildCard = buttonStartLeft;
-                    break;
-                case Position.Center:
-                    textBoxWildCard = textBoxURLCenter;
-                    buttonWildCard = buttonStartCenter;
-                    break;
-                case Position.Right:
-                    textBoxWildCard = textBoxURLRight;
-                    buttonWildCard = buttonStartRight;
-                    break;
-            }
-
-            buttonWildCard.IsEnabled = textBoxWildCard.Text.Length != 0;
+            startButton.IsEnabled = textBox.Text.Length != 0;
         }
 
         private void PreventNoURLDownloadAll()
@@ -69,134 +72,90 @@ namespace TestTask
             buttonDownloadAll.IsEnabled = textBoxURLLeft.Text.Length != 0 && textBoxURLCenter.Text.Length != 0 && textBoxURLRight.Text.Length != 0;
         }
 
-        private async Task StartDownload(Position position, string url, Downloader downloader)
+        private async Task StartDownload(Image image, Button startButton, Button stopButton, TextBox textBox, string fileName,Downloader downloader)
         {
-            Image imageWildCard = new Image();
-            Button stopButtonWildCard = new Button();
-            Button startButtonWildCard = new Button();
-            string fileName = "";
-            switch (position)
-            {
-                case Position.Left:
-                    imageWildCard = imageLeft;
-                    stopButtonWildCard = buttonStopLeft;
-                    startButtonWildCard = buttonStartLeft;
-                    fileName = "Left";
-                    break;
-                case Position.Center:
-                    imageWildCard = imageCenter;
-                    stopButtonWildCard = buttonStopCenter;
-                    startButtonWildCard = buttonStartCenter;
-                    fileName = "Center";
-                    break;
-                case Position.Right:
-                    imageWildCard = imageRight;
-                    stopButtonWildCard = buttonStopRight;
-                    startButtonWildCard = buttonStartRight;
-                    fileName = "Right";
-                    break;
-            }
 
-
-            imageWildCard.Source = null;
+            image.Source = null;
             buttonDownloadAll.IsEnabled = false;
-            stopButtonWildCard.IsEnabled = true;
-            startButtonWildCard.IsEnabled = false;
+            stopButton.IsEnabled = true;
+            startButton.IsEnabled = false;
             try
             {
-                imageWildCard.Source = await downloader.DownloadImage(url, fileName);
+                image.Source = await downloader.DownloadImage(textBox.Text, fileName);
             }
             catch(Exception e)
             {
                 MessageBox.Show(e.Message);
-                imageWildCard.Source = null;
+                image.Source = null;
             }
 
-            
-            stopButtonWildCard.IsEnabled = false;
-            startButtonWildCard.IsEnabled = true;
-            PreventNoURLStart(position);
-            PreventNoURLDownloadAll();
+            stopButton.IsEnabled = false;
+            startButton.IsEnabled = true;
             buttonDownloadAll.IsEnabled = LeftDownloader.State == Downloader.States.AwaitingDownload &&
             CenterDownloader.State == Downloader.States.AwaitingDownload &&
             RightDownloader.State == Downloader.States.AwaitingDownload;
+            PreventNoURLStart(textBox, startButton );
+            PreventNoURLDownloadAll();
         }
 
-        private void StopDownload(Position position)
+        private void StopDownload(Button stopButton, Button startButton, Downloader downloader)
         {
-            Button stopButtonWildCard = new Button();
-            Button startButtonWildCard = new Button();
-            switch (position)
-            {
-                case Position.Left:
-                    LeftDownloader.StopDownload();
-                    break;
-                case Position.Center:
-                    CenterDownloader.StopDownload();
-                    break;
-                case Position.Right:
-                    RightDownloader.StopDownload();
-                    break;
-            }
-
-            stopButtonWildCard.IsEnabled = false;
-            startButtonWildCard.IsEnabled = true;
         }
 
         private void textBoxURLLeft_TextChanged(object sender, TextChangedEventArgs e)
         {
-            PreventNoURLStart(Position.Left);
+            PreventNoURLStart(textBoxURLLeft, buttonStartLeft);
             PreventNoURLDownloadAll();
         }
 
         private void textBoxURLCenter_TextChanged(object sender, TextChangedEventArgs e)
         {
-            PreventNoURLStart(Position.Center);
+            PreventNoURLStart(textBoxURLCenter, buttonStartCenter);
             PreventNoURLDownloadAll();
         }
 
         private void textBoxURLRight_TextChanged(object sender, TextChangedEventArgs e)
         {
-            PreventNoURLStart(Position.Right);
+            PreventNoURLStart(textBoxURLRight, buttonStartRight);
             PreventNoURLDownloadAll();
         }
 
         private void buttonStartLeft_Click(object sender, RoutedEventArgs e)
         {
-            StartDownload(Position.Left, textBoxURLLeft.Text, this.LeftDownloader);
+            StartDownload(imageLeft, buttonStartLeft, buttonStopLeft, textBoxURLLeft, "Left",this.LeftDownloader);
         }
 
         private void buttonStartCenter_Click(object sender, RoutedEventArgs e)
         {
-            StartDownload(Position.Center, textBoxURLCenter.Text, this.CenterDownloader);
+            StartDownload(imageCenter, buttonStartCenter, buttonStopCenter, textBoxURLCenter, "Center", this.CenterDownloader);
         }
 
         private void buttonStartRight_Click(object sender, RoutedEventArgs e)
         {
-            StartDownload(Position.Right, textBoxURLRight.Text, this.RightDownloader);
+            StartDownload(imageRight, buttonStartRight, buttonStopRight, textBoxURLRight, "Right", this.RightDownloader);
         }
 
         private void buttonDownloadAll_Click(object sender, RoutedEventArgs e)
         {
-            StartDownload(Position.Left, textBoxURLLeft.Text, this.LeftDownloader);
-            StartDownload(Position.Center, textBoxURLCenter.Text, this.CenterDownloader);
-            StartDownload(Position.Right, textBoxURLRight.Text, this.RightDownloader);
+            StartDownload(imageLeft, buttonStartLeft, buttonStopLeft, textBoxURLLeft, "Left", this.LeftDownloader);
+            StartDownload(imageCenter, buttonStartCenter, buttonStopCenter, textBoxURLCenter, "Center", this.CenterDownloader);
+            StartDownload(imageRight, buttonStartRight, buttonStopRight, textBoxURLRight, "Right", this.RightDownloader);
         }
         //not awaiting async methods is SO wrong, but I don't think I can do anything about it
 
         private void buttonStopLeft_Click(object sender, RoutedEventArgs e)
         {
-            StopDownload(Position.Left);
+            LeftDownloader.StopDownload();
         }
 
         private void buttonStopCenter_Click(object sender, RoutedEventArgs e)
         {
-            StopDownload(Position.Center);
+            CenterDownloader.StopDownload();
         }
 
         private void buttonStopRight_Click(object sender, RoutedEventArgs e)
         {
-            StopDownload(Position.Right);
+            RightDownloader.StopDownload();
         }
     }
 }
